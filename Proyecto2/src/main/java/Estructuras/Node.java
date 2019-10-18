f52a9917ce319bd37f6d0458934c7a151b518ec8
Sebastián Obando <sebasoj13@gmail.com>
2019-10-18 09:22:26 -0600

    Configuración y clase LQueue
    Ventana de configuracion y clase de cola y nodo

---
+++ Proyecto2/src/main/java/Estructuras/Node.java
@@ -0,0 +1,43 @@
+package Estructuras;
+
+public class Node<T> {
+	
+	//atributos
+	
+	private T element;
+	private Node<T> next;
+	
+	//Constructores
+	public Node() {
+		this.element = null;
+		this.next = null;
+	}
+	
+	public Node(T element) {
+		this.element = element;
+		this.next = null;
+	}
+	
+	public Node(T element, Node<T> next) {
+		this.element = element;
+		this.next = next;
+	}
+	
+	//metodos
+	
+	public T getElement() {
+		return this.element;
+	}
+	
+	public void setElement(T element) {
+		this.element = element;
+	}
+	
+	public Node<T> getNext() {
+		return this.next;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;	
+	}
+}
